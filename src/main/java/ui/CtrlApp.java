ec3b010ac4ef12ef62dccf238647ba4eb373c27c
DuanYue166 <39989186+DuanYue166@users.noreply.github.com>
2023-07-04 22:57:26 +0800

    Update make-order page

--- src/main/java/ui/CtrlApp.java
+++ src/main/java/ui/CtrlApp.java
@@ -1,18 +1,28 @@
 package ui;
 
+import io.github.palexdev.materialfx.controls.MFXIconWrapper;
+import io.github.palexdev.materialfx.controls.MFXRectangleToggleNode;
+import io.github.palexdev.materialfx.controls.MFXScrollPane;
 import io.github.palexdev.mfxresources.fonts.MFXFontIcon;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.fxml.Initializable;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import ui.controllers.MakeOrderController;
 
 public class CtrlApp implements Initializable {
 
@@ -21,10 +31,8 @@ public class CtrlApp implements Initializable {
     private double yOffset;
     @FXML
     private MFXFontIcon closeIcon;
-
     @FXML
     private MFXFontIcon minimizeIcon;
-
     @FXML
     private VBox navBar;
     @FXML
@@ -32,14 +40,38 @@ public class CtrlApp implements Initializable {
     @FXML
     private HBox windowHeader;
     @FXML
-    private StackPane contentPane;
+    private MFXScrollPane contentPane;
+    private final ToggleGroup toggleGroup;
 
     public CtrlApp(Stage stage) {
         this.stage = stage;
+        this.toggleGroup = new ToggleGroup();
+    }
+
+    private ToggleButton createToggle(String icon, String text, double rotate) {
+        MFXIconWrapper wrapper = new MFXIconWrapper(icon, 24, 32);
+        MFXRectangleToggleNode toggleNode = new MFXRectangleToggleNode(text, wrapper);
+        toggleNode.setAlignment(Pos.CENTER_LEFT);
+        toggleNode.setMaxWidth(Double.MAX_VALUE);
+        toggleNode.setToggleGroup(toggleGroup);
+        if (rotate != 0) wrapper.getIcon().setRotate(rotate);
+        return toggleNode;
+    }
+
+    private Parent loadView(String fxmlName, Callback<Class<?>, Object> controllerFactory){
+        FXMLLoader loader = new FXMLLoader(App.class.getResource(fxmlName));
+        loader.setControllerFactory(controllerFactory);
+        try {
+            return loader.load();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(URL location, ResourceBundle resources){
         closeIcon.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> Platform.exit());
         minimizeIcon.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> ((Stage) rootPane.getScene().getWindow()).setIconified(true));
         windowHeader.setOnMousePressed(event -> {
@@ -52,15 +84,20 @@ public class CtrlApp implements Initializable {
         });
 
 
-    }
-
-    @FXML
-    void close(MouseEvent event) {
-
-    }
-
-    @FXML
-    void minimize(MouseEvent event) {
-
+        Parent view_makeOrder = loadView("fxml/MakeOrder.fxml", c -> new MakeOrderController());
+        ToggleButton toggle_makeOrder = createToggle("mfx-angle-right", "签订订单", 0);
+        toggle_makeOrder.setOnAction(event -> contentPane.setContent(view_makeOrder));
+        navBar.getChildren().add(toggle_makeOrder);
     }
 }
+/*
+FONTAWESOME_BRANDS("FontAwesome/brands/FontAwesomeBrands.ttf", FontAwesomeBrands::toCode),
+FONTAWESOME_REGULAR("FontAwesome/regular/FontAwesomeRegular.ttf", FontAwesomeRegular::toCode),
+FONTAWESOME_SOLID("FontAwesome/solid/FontAwesomeSolid.ttf", FontAwesomeSolid::toCode),
+
+&#xe988;//写订单
+&#xe989;//查看订单
+&#xe98a;//查看订单
+&#xe900;//用户
+&#xe901;//用户
+ */
---
+++ src/main/java/ui/controllers/MakeOrderController.java
@@ -0,0 +1,11 @@
+package ui.controllers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * author： Shuowei Hou
+ * date： 2023/7/4
+ * description：
+ */
+public class MakeOrderController {
+
+}
